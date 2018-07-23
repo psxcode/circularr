c0e1b583987eb430f6633c7f503b9275f9113883
psxcode <psxcode@gmail.com>
2018-07-23 21:33:18 +0200

    ✔️: add circularr impl

---
+++ src/index.spec.ts
@@ -0,0 +1,53 @@
+import { expect } from 'chai'
+import Circularr from './index'
+
+describe('[ Circularr ]', () => {
+  it('[ from ]', () => {
+    const arr = Circularr.from([1, 2, 3])
+    expect(Array.from(arr)).deep.eq([1, 2, 3])
+  })
+
+  it('[ length ]', () => {
+    const arr = Circularr.from([1, 2, 3])
+    expect(arr.length).eq(3)
+  })
+
+  it('[ fill ]', () => {
+    const arr = new Circularr<number>(3)
+    expect(Array.from(arr)).deep.eq([undefined, undefined, undefined])
+    arr.fill(42)
+    expect(Array.from(arr)).deep.eq([42, 42, 42])
+  })
+
+  it('[ shift ]', () => {
+    const arr = new Circularr(3).fill(0)
+    const res0 = arr.shift(42)
+    expect(Array.from(arr)).deep.eq([0, 0, 42])
+    expect(res0).eq(0)
+    const res1 = arr.shift(16)
+    expect(Array.from(arr)).deep.eq([0, 42, 16])
+    expect(res1).eq(0)
+    const res2 = arr.shift(32)
+    expect(Array.from(arr)).deep.eq([42, 16, 32])
+    expect(res2).eq(0)
+    const res3 = arr.shift(64)
+    expect(Array.from(arr)).deep.eq([16, 32, 64])
+    expect(res3).eq(42)
+  })
+
+  it('[ unshift ]', () => {
+    const arr = new Circularr(3).fill(0)
+    const res0 = arr.unshift(42)
+    expect(Array.from(arr)).deep.eq([42, 0, 0])
+    expect(res0).eq(0)
+    const res1 = arr.unshift(16)
+    expect(Array.from(arr)).deep.eq([16, 42, 0])
+    expect(res1).eq(0)
+    const res2 = arr.unshift(32)
+    expect(Array.from(arr)).deep.eq([32, 16, 42])
+    expect(res2).eq(0)
+    const res3 = arr.unshift(64)
+    expect(Array.from(arr)).deep.eq([64, 32, 16])
+    expect(res3).eq(42)
+  })
+})
---
+++ src/index.ts
@@ -0,0 +1,50 @@
+class Circularr <T> {
+  data: T[]
+  index = 0
+
+  constructor (length: number) {
+    this.data = new Array<T>(length)
+  }
+
+  static from <T> (source: T[]): Circularr<T> {
+    const c = new Circularr<T>(source.length)
+    for (let i = 0; i < source.length; ++i) {
+      c.data[i] = source[i]
+    }
+    return c
+  }
+
+  get length (): number {
+    return this.data.length
+  }
+
+  * [Symbol.iterator] () {
+    for (let i = 0; i < this.data.length; ++i) {
+      yield this.data[(i + this.index) % this.data.length]
+    }
+  }
+
+  fill (value: T): this {
+    for (let i = 0; i < this.data.length; ++i) {
+      this.data[i] = value
+    }
+    this.index = 0
+    return this
+  }
+
+  shift (value: T): T {
+    const returnValue = this.data[this.index]
+    this.data[this.index] = value
+    this.index = (this.index + 1) % this.data.length
+    return returnValue
+  }
+
+  unshift (value: T): T {
+    this.index = (this.index + this.data.length - 1) % this.data.length
+    const returnValue = this.data[this.index]
+    this.data[this.index] = value
+    return returnValue
+  }
+}
+
+export default Circularr
